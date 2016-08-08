efd3ecdc1034ff24622604c7392ae36a7d5f93fe
James Wu <james@jamesjwu.com>
2016-08-07 18:50:16 -0700

    added most of the tests for the journal class

--- tests.py
+++ tests.py
@@ -57,9 +57,13 @@ class TestS3Model(unittest.TestCase):
 class TestJournalClass(unittest.TestCase):
 
     def setUp(self):
-        self.journal = journal.Journal()
-        self.journal_entry_key = datetime.datetime.today()
+        self.test_user_id = "001"
+        self.journal = journal.Journal(self.test_user_id)
+        self.today_datetime = datetime.datetime.today()
         self.test_entry = "My name is Michael Weston."
+
+    def test_get_user_id(self):
+        self.assertEqual(self.journal.get_user_id(), self.test_user_id)
 
     def test_record_entry(self):
         """
@@ -71,22 +75,120 @@ class TestJournalClass(unittest.TestCase):
         # test that latest entry can be read
         pass
 
+    def test_record_entry_empty(self):
+        """
+            When provided an empty entry do not record
+        """
+        with self.assertRaises JournalEntryEmpty as e:
+            self.journal.record_entry(
+                self.today_datetime, "")
+
+    def test_record_entry_dupe(self):
+        """
+            Raises error when duplicate time stamps
+            entered
+        """
+        with self.assertRaises JournalEntryDupe as e:
+            self.journal.record_entry(
+                self.today_datetime, self.test_entry)
+            self.journal.record_entry(
+                self.today_datetime, self.test_entry)
+
     def test_read_entry(self):
         """
             Reads an entry when provided a datetime
+
+            Raise JournalEntryNotFound error if entry
+            missing
+        """
+        self.journal.record_entry(
+            self.today_datetime, self.test_entry)
+        read_value = self.journal.read_entry(self.today_datetime)
+        self.assertEqual(read_value, self.test_entry)
+        with self.assertRaises JournalEntryNotFound as e:
+            self.journal.read_entry(
+                self.today_datetime + datetime.timedelta(days=1))
+
+    def test_append_entry(self):
+        """
+            Raises JournalEntryNotFound if append
+            attempted
+        """
+        appended_text = " appended text"
+        with self.assertRaises JournalEntryNotFound as e:
+            self.journal.append_entry(
+                self.today_datetime, appended_text)
+        self.journal.record_entry(
+            self.today_datetime, self.test_entry)
+        self.assertEqual(
+            self.journal.read_entry(self.today_datetime),
+            self.test_entry)
+        self.journal.append_entry(
+            self.today_datetime, appended_text)
+        self.assertEqual(
+            self.journal.read_entry(self.today_datetime),
+            self.test_entry + self.appended_text)
+
+    def test_remove_entry(self):
+        """
+            Remove entry. Throw up error if not found
+        """
+        self.journal.record_entry(
+            self.today_datetime, self.test_entry)
+        self.assertEqual(
+            self.journal.read_entry(self.today_datetime),
+            self.test_entry)
+        self.journal.remove_entry(self.today_datetime)
+        with self.assertRaises JournalEntryNotFound as e:
+            self.journal.read_entry(self.today_datetime)
+        with self.assertRaises JournalEntryNotFound as e:
+            self.journal.remove_entry(self.today_datetime)
+
+    def test_get_latest_entry(self):
+        yester_datetime = self.today_datetime - datetime.timedelta(days = 1),
+        self.journal.record_entry(
+            yester_datetime,
+            self.test_entry)
+        self.assertEqual(self.get_latest_entry(),
+            (yester_datetime, self.test_entry))
+        self.journal.record_entry(
+            self.today_datetime,
+            "today's entry")
+        self.assertEqual(self.get_latest_entry(),
+            (self.today_datetime, "today's entry"))
+
+    def test_get_day_entries(self):
+        """
+            Return the entries from a given day
+            by local time
+        """
+        pass
+    def test_get_today_entries(self):
+        """
+            Return the entries from today by local time
         """
         pass
 
-    def test_remove_entry(self):
-        pass
-
-    def test_get_latest_entry(self):
-        pass
-
     def test_get_last_week_entries(self):
-        pass
+        """
+            Return the entries from last 7 days in order of
+            latest first
+        """
+        for ii in xrange(7)
+            ii_datetime = self.today_datetime - datetime.timedelta(days = ii)
+            self.journal.record_entry(
+                yester_datetime,
+                str(ii))
+        current_datetime = self.today_datetime
+        days_prev = 0
+        for entry_datetime, entry in self.get_last_week_entries():
+            self.assertEqual(entry_datetime, current_datetime)
+            self.assertEqual(entry, str(days_prev))
+            days_prev += 1
+            current_datetime += datetime.timedelta(days = 1)
 
     def test_get_last_month_entries(self):
         pass
 
-    def test_remove_la
+    def test_remove_all_entries(self):
+        pass
