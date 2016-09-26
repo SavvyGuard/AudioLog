d3a7a9c2f992219421953462ae9b5e553f9a7095
James Wu <james@jamesjwu.com>
2016-09-25 22:52:00 -0700

    created bones of test module for the high level skill itself

---
+++ tests/test_skill.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+import app
+
+class TestSkill(unittest.TestCase):
+
+    def __init__(self):
+        record_intent_filename = "test_requests/record_intent.json"
+        with open(record_intent_filename) as f:
+           record_intent = json.loads(f.read())
+        self.record_intent = record_intent
+
+    def setup(self):
+        pass
+
+    def record_new(self):
+        pass
+
+    def play_last(self):
+        pass
+
+    def play_all(self):
+        pass
